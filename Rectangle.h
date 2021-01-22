368008467fb332052bb0386df213a9707e742892
David Schreur <13521116+davidskur@users.noreply.github.com>
2021-01-22 11:57:20 -0500

    Exploring pointers and polymorphism

--- Rectangle.h
+++ Rectangle.h
@@ -1,13 +1,14 @@
 #ifndef _RECTANGLE
 #define _RECTANGLE
 
-class Rectangle {
+#include <iostream>
+#include "Shape.h"
+
+using namespace std;
+
+class Rectangle : public Shape {
 private:
 	double base, height, depth;
-	bool isValidShape() { // check for impossible dimensions
-		if (base <= 0 || height <= 0 || depth <= 0) return false;
-		return true;
-	}
 public:
 	Rectangle(double base, double height, double depth) { // parameterized constructor
 		this->base = base;
@@ -15,9 +16,13 @@ public:
 		this->depth = depth;
 	}
 	double getVolume() { // exposed public method to calculate volume
-		if (isValidShape())
+		if (isValidShape(base, height, depth))
 			return base * height * depth;
 		return 0;
+	}
+	void printInfo() { // override base class 
+		cout << "\nObject Type: Rectangle";
+		cout << "\nVolume: " << getVolume();
 	}
 };
 #endif
